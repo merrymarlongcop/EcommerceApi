4260f62cb762a43b46054533c49bfead6cc9f9c5
delarosa_longcop <merrymarlongcop8@gmail.com>
2026-05-13 23:33:56 +0800

    feat: implemented Spring Security with session-based auth

---
+++ EcommerceApi/src/main/java/com/ws101/delarosa_longcop/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.ws101.delarosa_longcop.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http)
+            throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/api/v1/products/**",
+                                "/api/v1/auth/**"
+                        ).permitAll()
+
+                        .anyRequest().authenticated()
+                )
+
+                .formLogin(form -> form.permitAll());
+
+        return http.build();
+    }
+}
